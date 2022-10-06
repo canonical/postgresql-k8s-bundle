e74376ae7303c9d056c8d95fae5238ca7102ab4a
WRFitch <will.fitch@canonical.com>
2022-10-06 17:21:37 +0100

    fixing unit addresses

--- tests/integration/bundle/test_bundle.py
+++ tests/integration/bundle/test_bundle.py
@@ -5,16 +5,19 @@ import asyncio
 import logging
 
 import pytest
+from lightkube import AsyncClient
+from lightkube.resources.core_v1 import Pod
 from pytest_operator.plugin import OpsTest
 
 from constants import PG, PGB
 from tests.integration.helpers.helpers import (
     deploy_postgres_k8s_bundle,
     get_app_relation_databag,
+    get_cfg,
     get_connecting_relations,
     wait_for_relation_joined_between,
 )
-from tests.integration.helpers.postgresql_helpers import execute_query_on_unit
+from tests.integration.helpers.postgresql_helpers import execute_query_on_unit, get_unit_address
 
 logger = logging.getLogger(__name__)
 
@@ -36,13 +39,49 @@ async def test_setup(ops_test: OpsTest):
         )
         await ops_test.model.add_relation(f"{PGB}:db", f"{FINOS_WALTZ}:db")
         wait_for_relation_joined_between(ops_test, PGB, FINOS_WALTZ)
-        await ops_test.model.wait_for_idle(apps=[PG, PGB, FINOS_WALTZ], status="active", timeout=1000)
+        await ops_test.model.wait_for_idle(
+            apps=[PG, PGB, FINOS_WALTZ], status="active", timeout=1000
+        )
 
 
 @pytest.mark.bundle
 async def test_kill_pg_primary(ops_test: OpsTest):
     """Kill postgres primary, check that all proxy instances switched traffic for a new primary."""
+    # Get primary connection string from each pgbouncer unit and assert they're pointing to the
+    # correct PG primary
+    old_primary_address = None
+    for unit in ops_test.model.applications[PGB].units:
+        unit_cfg = await get_cfg(ops_test, unit.name)
+        if not old_primary_address:
+            unit_cfg["databases"]["waltz"]["host"] = old_primary_address
+        assert unit_cfg["databases"]["waltz"]["host"] == old_primary_address
+        assert unit_cfg["databases"]["waltz_standby"]["host"] != old_primary_address
 
+    async with ops_test.fast_forward():
+        # Get postgres primary through action
+        unit_name = ops_test.model.applications[PG].units[0].name
+        action = await ops_test.model.units.get(unit_name).run_action("get-primary")
+        action = await action.wait()
+        primary = action.results["primary"]
+        # Delete the primary pod.
+        model = ops_test.model.info
+        client = AsyncClient(namespace=model.name)
+        await client.delete(Pod, name=primary.replace("/", "-"))
+        logger.info(f"deleted pod {primary}")
+        await ops_test.model.wait_for_idle(
+            apps=[PG, PGB, FINOS_WALTZ], status="active", timeout=600
+        )
+
+    new_primary_address = None
+    assert new_primary_address != old_primary_address
+    for unit in ops_test.model.applications[PGB].units:
+        unit_cfg = await get_cfg(ops_test, unit.name)
+        if not new_primary_address:
+            unit_cfg["databases"]["waltz"]["host"] = new_primary_address
+        assert unit_cfg["databases"]["waltz"]["host"] == new_primary_address
+        assert unit_cfg["databases"]["waltz"]["host"] != old_primary_address
+        assert unit_cfg["databases"]["waltz_standby"]["host"] != new_primary_address
+        assert unit_cfg["databases"]["waltz_standby"]["host"] != old_primary_address
 
 
 @pytest.mark.bundle
@@ -68,27 +107,27 @@ async def test_read_distribution(ops_test: OpsTest):
     assert None not in [pgpass, user, host, port, dbname], "databag incorrectly populated"
     dbname = f"{dbname}_standby"
 
-    user_command = "SELECT reset_val FROM pg_settings WHERE name='listen_addresses';"
+    query = "SELECT reset_val FROM pg_settings WHERE name='listen_addresses';"
 
     pgb_unit = ops_test.model.applications[PGB].units[0]
     # get first ip
     rtn, first_ip, err = await execute_query_on_unit(
-        unit_address=pgb_unit.public_address,
+        unit_address=get_unit_address(pgb_unit.name),
         user=user,
         password=pgpass,
-        query=user_command,
+        query=query,
         database=dbname,
     )
-    assert rtn == 0, f"failed to run admin command {user_command}, {err}"
+    assert rtn == 0, f"failed to run admin command {query}, {err}"
 
     # get second IP
     rtn, second_ip, err = await execute_query_on_unit(
-        unit_address=pgb_unit.public_address,
+        unit_address=get_unit_address(pgb_unit.name),
         user=user,
         password=pgpass,
-        query=user_command,
+        query=query,
         database=dbname,
     )
-    assert rtn == 0, f"failed to run admin command {user_command}, {err}"
+    assert rtn == 0, f"failed to run admin command {query}, {err}"
 
     assert first_ip != second_ip
