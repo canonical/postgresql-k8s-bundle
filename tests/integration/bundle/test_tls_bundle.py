f25911bca914eb6fe67ee8f5859bca1abade67bd
Dragomir Penev <6687393+dragomirp@users.noreply.github.com>
2023-04-09 17:10:41 +0300

    Update test_tls_bundle.py

--- tests/integration/bundle/test_tls_bundle.py
+++ tests/integration/bundle/test_tls_bundle.py
@@ -24,7 +24,7 @@ from ..helpers.postgresql_helpers import (
 logger = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
-MATTERMOST = "mattermost-k8s"
+FINOS_WALTZ = "finos-waltz-k8s"
 TLS = "tls-certificates-operator"
 RELATION = "backend-database"
 
@@ -33,7 +33,7 @@ async def test_tls_encrypted_connection_to_postgres(ops_test: OpsTest):
     # Relate PgBouncer to PostgreSQL.
     await asyncio.gather(
         deploy_postgres_k8s_bundle(ops_test),
-        ops_test.model.deploy(MATTERMOST, application_name=MATTERMOST),
+        ops_test.model.deploy(FINOS_WALTZ, application_name=FINOS_WALTZ),
     )
 
     # Enable additional logs on the PostgreSQL instance to check TLS
@@ -41,15 +41,12 @@ async def test_tls_encrypted_connection_to_postgres(ops_test: OpsTest):
     await enable_connections_logging(ops_test, f"{PG}/0")
 
     # Relate finos to PgBouncer to open a connection between PgBouncer and PostgreSQL.
-    await ops_test.model.add_relation(f"{PGB}:db", f"{MATTERMOST}:db")
+    await ops_test.model.add_relation(f"{PGB}:db", f"{FINOS_WALTZ}:db")
     async with ops_test.fast_forward():
         await ops_test.model.wait_for_idle(
-            apps=[PG, PGB, TLS_APP_NAME], status="active", timeout=600
+            apps=[PG, PGB, FINOS_WALTZ, TLS_APP_NAME], status="active", timeout=600
         )
-        # Mattermost could be in error state while creating the pod
-        await ops_test.model.wait_for_idle(
-            apps=[MATTERMOST], raise_on_error=False, status="active", timeout=600
-        )
+
 
     # Check the logs to ensure TLS is being used by PgBouncer.
     postgresql_primary_unit = await get_postgres_primary(ops_test)
@@ -58,5 +55,5 @@ async def test_tls_encrypted_connection_to_postgres(ops_test: OpsTest):
     )
     username, _ = await get_backend_user_pass(ops_test, get_backend_relation(ops_test))
     assert (
-        f"connection authorized: user={username} database=mattermost SSL enabled" in logs
+        f"connection authorized: user={username} database=waltz SSL enabled" in logs
     ), "TLS is not being used on connections to PostgreSQL"
